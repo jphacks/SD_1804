c6d4b925bca9b2944b9c18f89979c1ca16ae308c
r-kazama <narusai97@gmail.com>
2018-10-20 14:52:44 +0900

    post message

--- SmartPost-Server/functions/index.js
+++ SmartPost-Server/functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
@@ -12,5 +16,66 @@ exports.addMemo = functions.https.onRequest((req, res) => {
         res.status(405).send('Method Not Allowed');
         return;
     }
-    res.status(200).send(req.body);
+    // JsonParse
+    const obj = JSON.parse(req.body);
+    const src = obj.src;
+
+    res.status(200).send(obj);
 });
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.database().ref('/messages').push({ original: original }).then((snapshot) => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+        return res.redirect(303, snapshot.ref.toString());
+    });
+});
+
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+        // Grab the current value of what was written to the Realtime Database.
+        const original = snapshot.val();
+        console.log('Uppercasing', context.params.pushId, original);
+        const uppercase = original.toUpperCase();
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+        // writing to the Firebase Realtime Database.
+        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+        return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
+
+exports.getImages = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const id = req.query.id;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.database().ref(`/users/${id}/images`).once('value').then(data => {
+        res.status(200).send(data)
+    })
+    .catch(error => {
+        res.status(404).send({ message: 'Not Found' })
+    })
+});
+
+exports.addImages = functions.https.onRequest((req, res) => {
+    // JsonParse
+    obj = JSON.parse(req.body);
+    src = obj.src;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.database().ref(`/users/${id}/images`).push(req).then((snapshot) => {
+        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+        return res.redirect(303, snapshot.ref.toString());
+    });
+});
+
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+        // Grab the current value of what was written to the Realtime Database.
+        const original = snapshot.val();
+        console.log('Uppercasing', context.params.pushId, original);
+        const uppercase = original.toUpperCase();
+        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+        // writing to the Firebase Realtime Database.
+        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+        return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
