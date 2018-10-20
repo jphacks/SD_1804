ef3e9827afe400c3aaa944b409bcc30e40037f51
Taichi Moriya <minecraft0125@gmail.com>
2018-10-20 13:09:57 +0900

    add ui

--- SmartPost-iOS/NoName/Module/ViewControllers/MailList/MailListViewController.swift
+++ SmartPost-iOS/NoName/Module/ViewControllers/MailList/MailListViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 
 class MailListViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
+    let samples = ["test1","test2","test3","test4","test5","test6","test7","test8","test9","test10","test11","test12"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +21,15 @@ class MailListViewController: UIViewController,UICollectionViewDelegate,UICollec
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let test:UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "test", for: indexPath)
+        let imageView = test.contentView.viewWithTag(1) as! UIImageView
+        let cellImage = UIImage(named: samples[indexPath.row])
+        imageView.image = cellImage
+        return test
     }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return samples.count
+    }
+
     private func setupUI() {
         
     }
