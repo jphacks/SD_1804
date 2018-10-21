3b449f51c39aff95facf12dd0f90f442d346e3cf
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-10-21 13:18:54 +0900

    push notification

--- SmartPost-iOS/NoName/AppDelegate.swift
+++ SmartPost-iOS/NoName/AppDelegate.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
-import CoreData
+import Firebase
+import UserNotifications
+import FirebaseMessaging
 
 @UIApplicationMain
 
@@ -44,6 +46,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         tabBarController.tabBar.tintColor = .red
         window?.rootViewController = tabBarController
 
-        return true
-    }}
+//        let notificationSettings = UIUserNotificationSettings(
+//            types: [.badge, .sound, .alert], categories: nil
+//        )
+//        application.registerUserNotificationSettings(notificationSettings)
+        FirebaseApp.configure()
 
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+        return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = Messaging.messaging().fcmToken
+        print("FCM token: \(token ?? "")")
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+//        if let messageID = userInfo[gcmMessageIDKey] {
+//            print("Message ID: \(messageID)")
+//        }
+
+        // Print full message.
+        print(userInfo)
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("Firebase registration token: \(fcmToken)")
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+
+    func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+        debugLog(messaging, remoteMessage)
+    }
+}
